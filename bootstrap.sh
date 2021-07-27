4365dac9c30e7f54558cead1402c582989651609
wolf-yuan-6115 <h1030035@gmail.com>
2021-07-27 17:46:48 +0800

    Test new workflow

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,16 @@
-pm2-runtime --secret $PM2_SECRET --public $PM2_PUBLIC --machine-name "Black cat Server" --deep-monitoring start /home/process.json
+if [ "$1" == "test" ]
+then
+  for file in commands/*.js ; do 
+    node --check "$file"
+    if [ "$?" != "0" ]
+    then
+      echo "Check failed on $file"
+      exit 1
+    fi
+    node --check index.js
+    node --check util/Util.js
+    node --check include/play.js
+  done
+else
+  pm2-runtime --secret $PM2_SECRET --public $PM2_PUBLIC --machine-name "Black cat Server" --deep-monitoring start /home/process.json
+fi
