b2e26e27127cefb54b408480c3b0e9a9cd388f65
wolf-yuan-6115 <h1030035@gmail.com>
2021-08-25 00:42:13 -0700

    Fix ping command error

--- commands/ping.js
+++ commands/ping.js
@@ -8,8 +8,8 @@ module.exports = {
   },
   slashReply: true,
   async execute(message) {
-    let sent = await message.reply("Ping...").catch(console.error);
-    let api = sent.createdTimestamp - message.createdTimestamp;
+    await message.reply("Ping...").catch(console.error);
+    let api = Date.now() - message.createdTimestamp;
     message.editReply(`🏓 ┃ Pong! API:${api}ms WebSocket:${message.client.ws.ping}ms Database:...ms`).catch(console.error);
     let dbPing = await message.client.db.ping();
     message.editReply(`🏓 ┃ Pong! API:${api}ms WebSocket:${message.client.ws.ping}ms Database:${dbPing.average}ms`).catch(console.error);
